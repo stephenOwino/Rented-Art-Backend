2d11199bbbfb76acfe2dbb45f3ddd4fb23e3e29f
stephenOwino <stephenowin233@gmail.com>
2025-01-08 17:20:26 +0300

    Added CustomUserDetails and CustomUserDetailsService, updated SecurityConfig

--- src/main/java/com/stephenowino/Rented_Art_Backend/Security/SecurityConfig.java
+++ src/main/java/com/stephenowino/Rented_Art_Backend/Security/SecurityConfig.java
@@ -1,20 +1,56 @@
 package com.stephenowino.Rented_Art_Backend.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig implements WebMvcConfigurer {
+
+        @Autowired
+        UserDetailsService userDetailsService;
+
+//        @Autowired
+//        private JwtFilter jwtFilter;
 
         @Bean
-        public PasswordEncoder passwordEncoder() {
-                return new BCryptPasswordEncoder();
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                return http
+                        .csrf(customizer ->customizer.disable())
+                        .authorizeHttpRequests(request->request
+                                .requestMatchers("register","login")
+                                .permitAll()
+                                .anyRequest().authenticated())
+                        // http.formLogin(Customizer.withDefaults());
+                        .httpBasic(Customizer.withDefaults())//to avoid returning form while testing in postman(rest clients)
+                        .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))//making http stateless
+                        //to generate a new session id everytime the user makes a request
+                        //.addFilterBefore( jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                        .build();
+        }
+        @Bean
+        public AuthenticationProvider authenticationProvider(){
+                DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+                provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+                provider.setUserDetailsService(userDetailsService);
+
+                return provider;
         }
 
         @Bean
@@ -22,14 +58,14 @@ public class SecurityConfig {
                 return config.getAuthenticationManager();
         }
 
-//        @Bean
-//        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//                http.csrf().disable()
-//                        .authorizeHttpRequests()
-//                        .requestMatchers("/api/auth/**").permitAll() // Allow public access to authentication endpoints
-//                        .anyRequest().authenticated() // Protect all other endpoints
-//                        .and()
-//                        .httpBasic(); // Use basic authentication for testing
-//                return http.build();
-//        }
+        // CORS Configuration to handle cross-origin requests globally
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Allow all paths
+                        .allowedOrigins("https://rented-art.onrender.com") // Your hosted frontend URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
+                        .allowedHeaders("*") // Allow all headers
+                        .allowCredentials(true); // Allow credentials
+        }
+
 }
