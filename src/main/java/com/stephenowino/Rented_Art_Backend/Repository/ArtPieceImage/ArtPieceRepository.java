cb33a2ac9726cf0e8fa54c429078a40ef28a9627
stephenOwino <stephenowin233@gmail.com>
2025-01-20 22:37:02 +0300

    added @Repository configuration

--- src/main/java/com/stephenowino/Rented_Art_Backend/Repository/ArtPieceImage/ArtPieceRepository.java
+++ src/main/java/com/stephenowino/Rented_Art_Backend/Repository/ArtPieceImage/ArtPieceRepository.java
@@ -2,9 +2,11 @@ package com.stephenowino.Rented_Art_Backend.Repository.ArtPieceImage;
 
 import com.stephenowino.Rented_Art_Backend.Entity.ArtPiece;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ArtPieceRepository extends JpaRepository<ArtPiece, Long> {
 
         List<ArtPiece> findByCategoryName(String category);
